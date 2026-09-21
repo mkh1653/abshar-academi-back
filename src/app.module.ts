42be201a5439318b072598a902c4316610dc39b1
Mahdi <37782014+mkh1653@users.noreply.github.com>
2026-09-21 18:47:14 +0330

    feat: register auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './config/database.config';
+import { AuthModule } from './modules/auth/auth.module';
 import { AttendanceModule } from './modules/attendance/attendance.module';
 import { AcademyModule } from './modules/academy/academy.module';
 import { CoachesModule } from './modules/coaches/coaches.module';
@@ -21,6 +22,7 @@ import { UsersModule } from './modules/users/users.module';
       useFactory: databaseConfig,
     }),
     UsersModule,
+    AuthModule,
     CoachesModule,
     PlayersModule,
     AcademyModule,
