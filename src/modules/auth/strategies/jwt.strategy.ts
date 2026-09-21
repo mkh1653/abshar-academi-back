1d6ba65d43059218ce0f33627637a8cfb5d7e24b
Mahdi <37782014+mkh1653@users.noreply.github.com>
2026-09-21 18:47:37 +0330

    fix: return unauthorized for invalid access tokens

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
@@ -25,13 +25,13 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   async validate(payload: AccessTokenPayload): Promise<User> {
     if (payload.type !== 'access') {
-      throw new Error('Invalid access token');
+      throw new UnauthorizedException('Invalid access token');
     }
 
     const user = await this.usersService.findById(payload.sub);
 
     if (!user || !user.isActive) {
-      throw new Error('User is not active');
+      throw new UnauthorizedException('User is not active');
     }
 
     return user;
