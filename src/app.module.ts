79a47f55f611748b204aa7f8a7936af4969da617
Mahdi <37782014+mkh1653@users.noreply.github.com>
2026-09-21 18:45:20 +0330

    feat: register core domain modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './config/database.config';
+import { AttendanceModule } from './modules/attendance/attendance.module';
+import { AcademyModule } from './modules/academy/academy.module';
+import { CoachesModule } from './modules/coaches/coaches.module';
+import { DocumentsModule } from './modules/documents/documents.module';
+import { EvaluationsModule } from './modules/evaluations/evaluations.module';
+import { PlayersModule } from './modules/players/players.module';
+import { ProgressModule } from './modules/progress/progress.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
@@ -12,6 +20,14 @@ import { databaseConfig } from './config/database.config';
     TypeOrmModule.forRootAsync({
       useFactory: databaseConfig,
     }),
+    UsersModule,
+    CoachesModule,
+    PlayersModule,
+    AcademyModule,
+    AttendanceModule,
+    EvaluationsModule,
+    ProgressModule,
+    DocumentsModule,
   ],
 })
 export class AppModule {}
