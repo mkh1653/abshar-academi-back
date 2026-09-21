ba85a41931a6822438f5439e7fb8c9fe03383079
Mahdi <37782014+mkh1653@users.noreply.github.com>
2026-09-21 18:07:02 +0330

    chore: add backend foundation

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfig } from './config/database.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: databaseConfig,
+    }),
+  ],
+})
+export class AppModule {}
