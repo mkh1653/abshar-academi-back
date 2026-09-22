df1998d7ed2de23c8ed7a2b259b5732aaf361ff9
Mahdi <37782014+mkh1653@users.noreply.github.com>
2026-09-22 16:27:35 +0330

    fix: validate training group time format

--- src/modules/academy/dto/create-training-group.dto.ts
+++ src/modules/academy/dto/create-training-group.dto.ts
@@ -9,7 +9,7 @@ export class CreateTrainingGroupDto {
   @IsString() coachId!: string;
   @IsEnum(ClassGender) gender!: ClassGender;
   @IsEnum(TrainingDay) trainingDay!: TrainingDay;
-  @Matches(/^([01]d|2[0-3]):[0-5]d$/) startTime!: string;
+  @Matches(/^([01]\\d|2[0-3]):[0-5]\\d$/) startTime!: string;
   @Matches(/^([01]d|2[0-3]):[0-5]d$/) endTime!: string;
   @IsOptional() @IsInt() @Min(1) capacity?: number;
 }
