6e30d54d0d2aa74d5b45a2aadb5a152c0cf5b634
Mahdi <37782014+mkh1653@users.noreply.github.com>
2026-09-22 16:35:34 +0330

    feat: add secure document storage and bootstrap utilities

--- src/modules/documents/documents.module.ts
+++ src/modules/documents/documents.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
+import { Player } from '../players/entities/player.entity';
+import { PlayerGuardian } from '../players/entities/player-guardian.entity';
 import { PlayerDocument } from './entities/player-document.entity';
+import { DocumentsController } from './documents.controller';
+import { DocumentsService } from './documents.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PlayerDocument])],
-  exports: [TypeOrmModule],
+  imports: [
+    TypeOrmModule.forFeature([PlayerDocument, Player, PlayerGuardian]),
+    AuthModule,
+  ],
+  controllers: [DocumentsController],
+  providers: [DocumentsService],
+  exports: [DocumentsService, TypeOrmModule],
 })
 export class DocumentsModule {}
