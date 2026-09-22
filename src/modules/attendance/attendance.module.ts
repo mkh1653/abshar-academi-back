a7bd439f3afd424754d9506a2e3999faf0b4edde
Mahdi <37782014+mkh1653@users.noreply.github.com>
2026-09-22 16:30:47 +0330

    feat: register attendance service

--- src/modules/attendance/attendance.module.ts
+++ src/modules/attendance/attendance.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
+import { Player } from '../players/entities/player.entity';
+import { PlayerGuardian } from '../players/entities/player-guardian.entity';
+import { TrainingGroup } from '../academy/entities/training-group.entity';
+import { TrainingSession } from '../academy/entities/training-session.entity';
 import { Attendance } from './entities/attendance.entity';
+import { AttendanceController } from './attendance.controller';
+import { AttendanceService } from './attendance.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Attendance])],
-  exports: [TypeOrmModule],
+  imports: [
+    TypeOrmModule.forFeature([
+      Attendance,
+      TrainingSession,
+      TrainingGroup,
+      Player,
+      PlayerGuardian,
+    ]),
+    AuthModule,
+  ],
+  controllers: [AttendanceController],
+  providers: [AttendanceService],
+  exports: [AttendanceService, TypeOrmModule],
 })
 export class AttendanceModule {}
