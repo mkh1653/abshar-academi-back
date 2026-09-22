2492a88cc2d4f93d294887b571e616b2a7d534ce
Mahdi <37782014+mkh1653@users.noreply.github.com>
2026-09-22 16:35:51 +0330

    feat: add swagger api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -14,6 +14,16 @@ async function bootstrap(): Promise<void> {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Abshar Volleyball Academy API')
+    .setDescription('Backend API for Abshar Volleyball Academy')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.PORT ? Number(process.env.PORT) : 3000);
 }
