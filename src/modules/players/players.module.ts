6502e695fd9142280b4bda2afaa74b64921ebde5
Mahdi <37782014+mkh1653@users.noreply.github.com>
2026-09-22 16:22:54 +0330

    feat: register player api providers

--- src/modules/players/players.module.ts
+++ src/modules/players/players.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
+import { Coach } from '../coaches/entities/coach.entity';
+import { Level } from '../academy/entities/level.entity';
+import { UsersModule } from '../users/users.module';
 import { Guardian } from './entities/guardian.entity';
 import { PlayerGuardian } from './entities/player-guardian.entity';
 import { Player } from './entities/player.entity';
+import { PlayersController } from './players.controller';
+import { PlayersService } from './players.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Player, Guardian, PlayerGuardian])],
-  exports: [TypeOrmModule],
+  imports: [
+    TypeOrmModule.forFeature([Player, Guardian, PlayerGuardian, Level, Coach]),
+    UsersModule,
+    AuthModule,
+  ],
+  controllers: [PlayersController],
+  providers: [PlayersService],
+  exports: [TypeOrmModule, PlayersService],
 })
 export class PlayersModule {}
