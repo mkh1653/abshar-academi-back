85327e3205b2e594ac17d2295486d5333fa4a16b
Mahdi <37782014+mkh1653@users.noreply.github.com>
2026-09-22 16:27:10 +0330

    feat: implement academy scheduling APIs

---
+++ src/modules/academy/dto/create-training-group.dto.ts
@@ -0,0 +1,15 @@
+import { IsEnum, IsInt, IsOptional, IsString, Matches, Min } from 'class-validator';
+import { ClassGender } from '../enums/class-gender.enum';
+import { TrainingDay } from '../enums/training-day.enum';
+
+export class CreateTrainingGroupDto {
+  @IsString() name!: string;
+  @IsOptional() @IsString() levelId?: string;
+  @IsString() hallId!: string;
+  @IsString() coachId!: string;
+  @IsEnum(ClassGender) gender!: ClassGender;
+  @IsEnum(TrainingDay) trainingDay!: TrainingDay;
+  @Matches(/^([01]d|2[0-3]):[0-5]d$/) startTime!: string;
+  @Matches(/^([01]d|2[0-3]):[0-5]d$/) endTime!: string;
+  @IsOptional() @IsInt() @Min(1) capacity?: number;
+}
