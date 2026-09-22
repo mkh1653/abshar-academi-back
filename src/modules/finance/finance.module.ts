9805202768c3adbc7cd4ae6814e90138420b93d8
Mahdi <37782014+mkh1653@users.noreply.github.com>
2026-09-22 16:26:40 +0330

    feat: register finance API module

---
+++ src/modules/finance/finance.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FinanceController } from './finance.controller';
+import { FinanceService } from './finance.service';
+import { MockPaymentGateway } from './mock-payment.gateway';
+import { Service } from './entities/service.entity';
+import { PlayerSubscription } from './entities/player-subscription.entity';
+import { Invoice } from './entities/invoice.entity';
+import { InvoiceItem } from './entities/invoice-item.entity';
+import { Payment } from './entities/payment.entity';
+import { Expense } from './entities/expense.entity';
+import { Player } from '../players/entities/player.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Service,
+      PlayerSubscription,
+      Invoice,
+      InvoiceItem,
+      Payment,
+      Expense,
+      Player,
+    ]),
+  ],
+  controllers: [FinanceController],
+  providers: [FinanceService, MockPaymentGateway],
+  exports: [FinanceService, TypeOrmModule],
+})
+export class FinanceModule {}
