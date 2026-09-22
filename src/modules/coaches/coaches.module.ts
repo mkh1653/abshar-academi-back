7c78b64dbc6ad6cd8fe881f5e8e7856edc9d037b
Mahdi <37782014+mkh1653@users.noreply.github.com>
2026-09-22 16:32:19 +0330

    feat: implement coach management API

--- src/modules/coaches/coaches.module.ts
+++ src/modules/coaches/coaches.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
+import { User } from '../users/entities/user.entity';
 import { Coach } from './entities/coach.entity';
+import { CoachesController } from './coaches.controller';
+import { CoachesService } from './coaches.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Coach])],
-  exports: [TypeOrmModule],
+  imports: [
+    TypeOrmModule.forFeature([Coach, User]),
+    AuthModule,
+  ],
+  controllers: [CoachesController],
+  providers: [CoachesService],
+  exports: [CoachesService, TypeOrmModule],
 })
 export class CoachesModule {}
