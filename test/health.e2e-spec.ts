0627c052f03fb22534ed18c6248cdee38349a720
Mahdi <37782014+mkh1653@users.noreply.github.com>
2026-09-22 18:02:47 +0330

    test: use compatible supertest import

--- test/health.e2e-spec.ts
+++ test/health.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request = require('supertest');
 import { HealthModule } from '../src/modules/health/health.module';
 
 describe('Health', () => {
@@ -19,7 +19,7 @@ describe('Health', () => {
   });
 
   it('returns ok', async () => {
-    await request.default(app.getHttpServer())
+    await request(app.getHttpServer())
       .get('/health')
       .expect(200)
       .expect((response) => {
