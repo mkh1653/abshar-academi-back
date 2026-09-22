66e623172f0c0a32ce6279d1a78febb7a8692ae6
Mahdi <37782014+mkh1653@users.noreply.github.com>
2026-09-22 18:18:26 +0330

    fix: require coach mobile number

--- src/modules/coaches/dto/create-coach.dto.ts
+++ src/modules/coaches/dto/create-coach.dto.ts
@@ -3,7 +3,7 @@ import { IsEmail, IsOptional, IsString, Matches, MinLength } from 'class-validat
 export class CreateCoachDto {
   @IsString() firstName!: string;
   @IsString() lastName!: string;
-  @IsOptional() @Matches(/^\+?[0-9]{10,15}$/) mobile?: string;
+  @Matches(/^\+?[0-9]{10,15}$/) mobile!: string;
   @IsOptional() @IsEmail() email?: string;
   @IsString() @MinLength(8) password!: string;
   @IsOptional() @IsString() bio?: string;
